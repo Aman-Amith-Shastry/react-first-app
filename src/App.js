94dc40b815006f72ac9554977a9082add8c90d2c
Aman-Amith-Shastry <73488475+Aman-a14@users.noreply.github.com>
2023-09-04 15:28:21 -0400

    react-routes

--- src/App.js
+++ src/App.js
@@ -1,64 +1,18 @@
 import NavBar from './Navbar';
-import Users from './Users';
-import { useState, useEffect } from 'react';
-import useFetch from './useFetch';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Body from './Body';
 
 function App() {
 
-  const [num, setNum] = useState(Math.random);
-  const title = "Hello!";
-  // const [blogs, setBlogs] = useState([
-  //   {title: "Blog 1", body: "Loren ipsum 1", id: 1},
-  //   {title: "Blog 2", body: "Loren ipsum 2", id: 2},
-  //   {title: "Blog 3", body: "Loren ipsum 3", id: 3},
-  // ]);
-
-
-  const {data, isPending, error, setData} = useFetch('http://localhost:8000/data');
-
-  const handleDelete = (id) => {
-    const newUsers = data.filter((user) => user.id !== id);
-    setData(newUsers);
-  }
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     fetch('http://localhost:8000/data').then(
-  //       res => {
-  //         if (!res.ok){
-  //           throw Error("Could not fetch data");
-  //         }
-  //         return res.json();
-  //       }
-  //     ).then(
-  //       (data) => {
-  //         setUsers(data);
-  //         setIsPending(false);
-  //         setError(null);
-  //       }
-  //     )
-  //     .catch((err) => {
-  //       setError(err.message);
-  //       setIsPending(false);
-  //     })
-  // }, 1000);
-  // }, []);
-
   return (
-
-    <div className="App">
-      <NavBar />
-      <div className = "content">
-        <h1> { title } </h1>
-        <p> { num } </p>
-        <button onClick = {() => {setNum(Math.random)}}> Generate random number! </button>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className = "content">
+            <Body/>
+        </div>
       </div>
-    
-    { error && <div className = "err"> { error } </div>}
-    { isPending && <div> Loading... </div>}
-    { data && <Users users = {data} handleDelete = {handleDelete}/> }
-      
-    </div>
+    </Router>
   );
 }
 
--- src/Body.js
+++ src/Body.js
@@ -1,12 +1,29 @@
+import Users from './Users';
+import { useState, useEffect } from 'react';
+import useFetch from './useFetch';
+
 const Body = () => {
+
+    const [num, setNum] = useState(Math.random);
+    const title = "Hello!";
+  
+    const {data: blogs, isPending, error, setData} = useFetch('http://localhost:8000/data');
+  
+    const handleDelete = (id) => {
+      const newUsers = blogs.filter((user) => user.id !== id);
+      setData(newUsers);
+    }
+
     return(
-        <body className = "body">
-            <button onClick={handleClick}> Click me! </button>
-            <div className = "content">
-                <h1> { title } </h1>
-                <p> { num } </p>
-            </div>
-        </body>
+        <div className = "content">
+            <h1> { title } </h1>
+            <p> { num } </p>
+
+        { error && <div className = "err"> { error } </div>}
+        { isPending && <div> Loading... </div>}
+        { blogs && <Users users = {blogs} handleDelete = {handleDelete}/> }
+        
+        </div>
     );
 }
 
